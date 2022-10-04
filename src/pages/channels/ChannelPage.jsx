a0b44496fe7b718769c5e2e14a288d94e67667af
Nguyen Le Phong <nguyenlephong1997@gmail.com>
2022-10-04 08:50:48 +0700

    Add channel page

---
+++ src/pages/channels/ChannelPage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const ChannelPage = props => {
+  return (
+    <div>
+      ChannelPage
+    </div>
+  );
+};
+
+ChannelPage.propTypes = {
+
+};
+
+export default ChannelPage;
