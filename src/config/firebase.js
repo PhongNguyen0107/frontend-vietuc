04d7990a17a822dcd9d08f2fef1589082206ada1
Nguyen Le Phong <nguyenlephong1997@gmail.com>
2022-10-04 08:48:57 +0700

    Setup firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,29 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { GoogleAuthProvider, getAuth } from "firebase/auth";
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD3d2qNGoTV5F0C0jhzmLZBiBoZamHhapw",
+  authDomain: "slack-ffd4f.firebaseapp.com",
+  projectId: "slack-ffd4f",
+  storageBucket: "slack-ffd4f.appspot.com",
+  messagingSenderId: "964565358562",
+  appId: "1:964565358562:web:5143f80cbd91047b3a0bbb",
+  measurementId: "G-5V390Z3KHY"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const provider = new GoogleAuthProvider();
+const auth = getAuth()
+
+export {analytics, provider, auth}
