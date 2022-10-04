fcbd02283ac31c866f72982aca927f71467c0435
Nguyen Le Phong <nguyenlephong1997@gmail.com>
2022-10-04 08:47:45 +0700

    Init project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './App.css';
+import SlackPublicRoutes from "routes/router";
+
+function App() {
+  return (
+   <SlackPublicRoutes/>
+  );
+}
+
+export default App;
